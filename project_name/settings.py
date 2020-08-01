26ee39df4c7b38eaa29ad5a1566776a4f861bd37
tyrainman427 <tyraineytech@gmail.com>
2020-08-01 16:06:07 -0400

    finishing touches

--- project_name/settings.py
+++ project_name/settings.py
@@ -24,7 +24,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = {{SECRET_KEY}}
+SECRET_KEY = "%ppjxiq8eyx=rj(0s(rzgziq&f@h0i!@gi1v1f2pw@yi4+an%0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
